8a35b2dca427e68046fbc41d93f307ecd7c7f323
Zodd <cryptozodd9@gmail.com>
2026-08-31 10:25:14 +0100

    Remove Selected Work projects section

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -7,7 +7,6 @@ import Landing from "./Landing";
 import Navbar from "./Navbar";
 import SocialIcons from "./SocialIcons";
 import WhatIDo from "./WhatIDo";
-import Work from "./Work";
 import setSplitText from "./utils/splitText";
 
 const TechStack = lazy(() => import("./TechStack"));
@@ -24,9 +23,7 @@ const MainContainer = ({ children }: PropsWithChildren) => {
     };
     resizeHandler();
     window.addEventListener("resize", resizeHandler);
-    return () => {
-      window.removeEventListener("resize", resizeHandler);
-    };
+    return () => window.removeEventListener("resize", resizeHandler);
   }, [isDesktopView]);
 
   return (
@@ -42,7 +39,6 @@ const MainContainer = ({ children }: PropsWithChildren) => {
             <About />
             <WhatIDo />
             <Career />
-            <Work />
             {isDesktopView && (
               <Suspense fallback={<div>Loading....</div>}>
                 <TechStack />
